4a4866e73aca7f9354e7ab5ec4ee080331d79f19
gty95 <gty95@192.168.204.128>
2019-08-07 23:56:07 +0800

    initialize springboot2.0 project

---
+++ src/main/java/com/gty/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.gty.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello spring boot";
+	}
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloController.class, args);
+	}
+}
